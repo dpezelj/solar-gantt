3e1f22a122909aa170805fe6d46c82d0cf9c2ed5
dominikpezelj <pezelj.dominik@gmail.com>
2023-06-11 18:20:34 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import { Gantt, Task, ViewMode } from 'gantt-task-react';
+import { getStartEndDateForProject, initTasks } from './helper';
+import "gantt-task-react/dist/index.css";
+import "./index.css"
+function App() {
+  const [tasks, setTasks] = useState<Task[]>(initTasks());
+  const columnWidth = 65;
+
+  const handleTaskChange = (task: Task) => {
+    console.log("On date change Id:" + task.id);
+    let newTasks = tasks.map(t => (t.id === task.id ? task : t));
+    if (task.project) {
+      const [start, end] = getStartEndDateForProject(newTasks, task.project);
+      const project = newTasks[newTasks.findIndex(t => t.id === task.project)];
+      if (
+        project.start.getTime() !== start.getTime() ||
+        project.end.getTime() !== end.getTime()
+      ) {
+        const changedProject = { ...project, start, end };
+        newTasks = newTasks.map(t =>
+          t.id === task.project ? changedProject : t
+        );
+      }
+    }
+    setTasks(newTasks);
+  };
+
+  const handleTaskDelete = (task: Task) => {
+    const conf = window.confirm("Are you sure about " + task.name + " ?");
+    if (conf) {
+      setTasks(tasks.filter(t => t.id !== task.id));
+    }
+    return conf;
+  };
+
+  const handleProgressChange = async (task: Task) => {
+    setTasks(tasks.map(t => (t.id === task.id ? task : t)));
+    console.log("On progress change Id:" + task.id);
+  };
+
+  const handleDblClick = (task: Task) => {
+    alert("On Double Click event Id:" + task.id);
+  };
+
+  const handleClick = (task: Task) => {
+    console.log("On Click event Id:" + task.id);
+  };
+
+  const handleSelect = (task: Task, isSelected: boolean) => {
+    console.log(task.name + " has " + (isSelected ? "selected" : "unselected"));
+  };
+
+  const handleExpanderClick = (task: Task) => {
+    setTasks(tasks.map(t => (t.id === task.id ? task : t)));
+    console.log("On expander click Id:" + task.id);
+  };
+
+  return (
+    <div>
+
+      <h3>Gantt With Unlimited Height</h3>
+      <Gantt
+        tasks={tasks}
+        onDateChange={handleTaskChange}
+        onDelete={handleTaskDelete}
+        onProgressChange={handleProgressChange}
+        onDoubleClick={handleDblClick}
+        onClick={handleClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={""}
+        columnWidth={columnWidth}
+      />
+      
+    </div>
+  );
+}
+
+export default App
